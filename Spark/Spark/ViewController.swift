cd00e3281b3cd9b74d555336f2a5f11e226e54a2
rkanjani <rkanj017@uottawa.ca>
2017-01-30 23:53:42 -0500

    Created project

---
+++ Spark/Spark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spark
+//
+//  Created by Rohan Kanjani on 2017-01-30.
+//  Copyright © 2017 Rohan Kanjani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
